0f918a6c7919f66f603aa13f3e2614dcc61e0714
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-06 17:04:35 +0100

    Day 1

---
+++ app/Day1.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Lib
+
+parse :: Char -> Int
+parse '(' = 1
+parse ')' = -1
+parse _ = 0
+
+solve1 :: [String] -> Int
+solve1 = sum . map parse . head
+
+solve2 :: [String] -> Int
+solve2 = length . takeWhile (/=(-1)) . scanl (+) 0 . map parse . head
+
+main :: IO()
+main = mainWrapper "day1" [solve1, solve2]
