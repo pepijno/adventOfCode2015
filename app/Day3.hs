55d3a08651fb38de34662be354b34418a0438c98
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-06 17:57:15 +0100

    Day 4

--- app/Day3.hs
+++ app/Day3.hs
@@ -10,19 +10,20 @@ moveSanta (x, y) 'v' = (x, y + 1)
 moveSanta (x, y) '<' = (x - 1, y)
 moveSanta pos _ = pos
 
+houses :: String -> [(Int, Int)]
 houses = scanl moveSanta (0,0)
 
 solve1 :: [String] -> Int
 solve1 = length . nub . houses . head
 
+extractMoves :: (Int -> Bool) -> [a] -> [a]
 extractMoves f = map snd . filter (f . fst) . zip [1..]
-extractSantaMoves = extractMoves odd
-extractRoboSantaMoves = extractMoves even
 
+solve2 :: [String] -> Int
 solve2 xs = length . nub $ (houses sms) ++ (houses rsms)
   where
-    sms  = extractSantaMoves $ head xs
-    rsms = extractRoboSantaMoves $ head xs
+    sms  = extractMoves odd $ head xs
+    rsms = extractMoves even $ head xs
 
 main :: IO()
 main = mainWrapper "day3" [solve1, solve2]
---
+++ app/Day4.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Lib
+import qualified Data.ByteString.Lazy.UTF8 as BLU
+import Data.Digest.Pure.MD5
+
+hasCorrectHash :: String -> Int -> Int -> Bool
+hasCorrectHash code n x = (==) zeroes . take n . show . md5 . BLU.fromString $ code ++ (show x)
+  where zeroes = take n (repeat '0')
+
+solve1 :: [String] -> Int
+solve1 xs = head . filter (hasCorrectHash code 5) $ [1..]
+  where code = head xs
+
+solve2 :: [String] -> Int
+solve2 xs = head . filter (hasCorrectHash code 6) $ [1..]
+  where code = head xs
+
+main :: IO()
+main = mainWrapper "day4" [solve1, solve2]
